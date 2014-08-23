72df30b883c402de512fac39936abb49ccd37ff3
Brian Hiatt <brian.hiatt41@gmail.com>
2014-08-22 20:15:30 -0500

    user create and login

---
+++ app/models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+function User(){
+}
+
+Object.defineProperty(User, 'collection', {
+  get: function(){return global.mongodb.collection('users');}
+});
+
+User.register = function(o, cb){
+  User.collection.findOne({email:o.email}, function(err, user){
+    if(user){return cb();}
+    o.password = bcrypt.hashSync(o.password, 10);
+    User.collection.save(o, cb);
+  });
+};
+
+User.authenticate = function(o, cb){
+  User.collection.findOne({email:o.email}, function(err, user){
+    if(!user){return cb();}
+    var isOk = bcrypt.compareSync(o.password, user.password);
+    if(!isOk){return cb();}
+    cb(user);
+  });
+};
+
+User.all = function(cb){
+  User.collection.find().toArray(cb);
+};
+
+module.exports = User;
+
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,6 +4,9 @@ var morgan         = require('morgan'),
     bodyParser     = require('body-parser'),
     methodOverride = require('express-method-override'),
     less           = require('less-middleware'),
+    session        = require('express-session'),
+    // requires connect-redis and passes in session
+    RedisStore     = require('connect-redis')(session),
     home           = require('../controllers/home'),
     users          = require('../controllers/users');
 
@@ -13,10 +16,20 @@ module.exports = function(app, express){
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(methodOverride());
+  // this opens up the cookie stored by the browswer and reads it
+  app.use(session({store:new RedisStore(),
+    secret:'jazzy hands',
+    resave:true,
+    saveUninitialized:true,
+    cookie:{maxAge:null}
+  }));
 
   app.get('/', home.index);
 
   app.get('/register', users.new);
+  app.post('/register', users.create);
+  app.get('/login', users.login);
+  app.post('/login', users.authenticate);
 
   console.log('Express: Routes Loaded');
 };
